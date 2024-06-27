736c8228b7ea11b09a9e3a029181e9d7f29028f6
Jack Muir <jmuirbusiness@gmail.com>
2024-06-27 21:14:03 +0100

    Commit 3

---
+++ app/properties/add/page.jsx
@@ -0,0 +1,15 @@
+import PropertyAddForm from "@/components/PropertyAddForm";
+
+const AddPropertiesPage = () => {
+  return (
+    <section class="bg-blue-50">
+      <div class="container m-auto max-w-2xl py-24">
+        <div class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AddPropertiesPage;
--- components/Meta.jsx
+++
@@ -1,5 +0,0 @@
-export const metadata = {
-  title: 'Property Pulse | Find the best property for you',
-  description: 'Find the best property for you',
-  keywords: 'property, real estate, buy, sell, rent',
-}
